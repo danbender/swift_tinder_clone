6ae20cd530aa6cc97f2a2495cbd44886c05fdf2a
Dan Bender <hello@danbender.net>
2016-01-06 21:37:49 -0500

    set up front and back card

--- SwiftTinderClone/CardsViewController.swift
+++ SwiftTinderClone/CardsViewController.swift
@@ -2,12 +2,23 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    let frontCardTopMargin: CGFloat = 0
+    let backCardTopMargin: CGFloat = 10
+    
     @IBOutlet weak var cardStackView: UIView!
+    
+    var backCard: SwipeView?
+    var frontCard: SwipeView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        backCard = SwipeView(frame: createCardFrame(backCardTopMargin))
+        cardStackView.addSubview(backCard!)
+        
+        frontCard = SwipeView(frame: createCardFrame(frontCardTopMargin))
+        cardStackView.addSubview(frontCard!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -15,5 +26,7 @@ class CardsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    private func createCardFrame(topMargin: CGFloat) -> CGRect {
+        return CGRect(x: 0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
+    }
 }
