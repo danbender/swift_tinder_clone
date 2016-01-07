96b8947f7c17ebed17bddfb600c6a11724f2107f
Dan Bender <hello@danbender.net>
2016-01-06 21:25:30 -0500

    storyboard setup for the cards view contoller

---
+++ SwiftTinderClone/CardsViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    @IBOutlet weak var cardStackView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
