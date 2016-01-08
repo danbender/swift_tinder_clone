ac329d7fdba4c00ba571c3826e72614477eef12a
Dan Bender <hello@danbender.net>
2016-01-08 18:07:12 +0100

    added rotation to cards, removed auto-layout because it messed up the rotation; reset after rotation

--- SwipeView.swift
+++ SwipeView.swift
@@ -26,12 +26,8 @@ class SwipeView: UIView {
         addSubview(card)
         
         self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "dragged:"))
-        
-        originalPoint = center
-        
-        card.setTranslatesAutoresizingMaskIntoConstraints(false)
-        
-        setConstraints()
+
+        card.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
     }
     
     func dragged(gestureRecognizer: UIPanGestureRecognizer) {
@@ -42,6 +38,10 @@ class SwipeView: UIView {
         case UIGestureRecognizerState.Began:
             originalPoint = center
         case UIGestureRecognizerState.Changed:
+            let rotationPercent = min(distance.x/(self.superview!.frame.width/2), 1)
+            let rotationAngle = (CGFloat(2*M_PI/16)*rotationPercent)
+            transform = CGAffineTransformRotate(transform, rotationAngle)
+            
             center = CGPointMake(originalPoint!.x + distance.x, originalPoint!.y + distance.y)
         case UIGestureRecognizerState.Ended:
             resetViewPositionAndTransformations()
@@ -54,14 +54,7 @@ class SwipeView: UIView {
     private func resetViewPositionAndTransformations() {
         UIView.animateWithDuration(0.1, animations: { () -> Void in
             self.center = self.originalPoint!
+            self.transform = CGAffineTransformMakeRotation(0)
         })
-    }
-    
-    
-    private func setConstraints() {
-        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0))
-        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0))
-        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0))
-        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0))
     }
 }
