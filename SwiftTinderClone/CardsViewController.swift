539129cc519d1e74cc82f245d4c14ea848bcda54
Dan Bender <hello@danbender.net>
2016-01-09 18:05:37 +0100

    implementing protocol functions to register and remove cards when swiped to left resp. right

--- SwiftTinderClone/CardsViewController.swift
+++ SwiftTinderClone/CardsViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class CardsViewController: UIViewController {
+class CardsViewController: UIViewController, SwipeViewDelegate {
 
     let frontCardTopMargin: CGFloat = 0
     let backCardTopMargin: CGFloat = 10
@@ -18,9 +18,11 @@ class CardsViewController: UIViewController {
         cardStackView.backgroundColor = UIColor.clearColor()
         
         backCard = SwipeView(frame: createCardFrame(backCardTopMargin))
+        backCard?.delegate = self
         cardStackView.addSubview(backCard!)
         
         frontCard = SwipeView(frame: createCardFrame(frontCardTopMargin))
+        frontCard!.delegate = self
         cardStackView.addSubview(frontCard!)
     }
 
@@ -31,5 +33,21 @@ class CardsViewController: UIViewController {
     
     private func createCardFrame(topMargin: CGFloat) -> CGRect {
         return CGRect(x: 0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
+    }
+    
+//    MARK: SwipeViewDelegate
+    
+    func swipedLeft() {
+        println("left")
+        if let frontCard = frontCard {
+            frontCard.removeFromSuperview()
+        }
+    }
+    
+    func swipedRight() {
+        println("right")
+        if let frontCard = frontCard {
+            frontCard.removeFromSuperview()
+        }
     }
 }
