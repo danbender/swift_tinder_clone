08bbd1fdcca715627ff75567537b2165532087d3
Dan Bender <hello@danbender.net>
2016-01-10 12:53:50 +0100

    Struct to encapsulate a CardView with a specific SwipeView so that the CardsViewController can manage both.

--- SwiftTinderClone/CardsViewController.swift
+++ SwiftTinderClone/CardsViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class CardsViewController: UIViewController, SwipeViewDelegate {
 
+    struct Card {
+        let cardView: CardView
+        let swipeView: SwipeView
+    }
+    
     let frontCardTopMargin: CGFloat = 0
     let backCardTopMargin: CGFloat = 10
     
@@ -34,6 +39,15 @@ class CardsViewController: UIViewController, SwipeViewDelegate {
     private func createCardFrame(topMargin: CGFloat) -> CGRect {
         return CGRect(x: 0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
     }
+    
+    private func createCard(topMargin: CGFloat) -> Card {
+        let cardView = CardView()
+        let swipeView = SwipeView(frame: createCardFrame(topMargin))
+        swipeView.delegate = self
+        swipeView.innerView = cardView
+        return Card(cardView: cardView, swipeView: swipeView)
+    }
+
     
 //    MARK: SwipeViewDelegate
     
