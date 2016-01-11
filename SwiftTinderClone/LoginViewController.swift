f6594ea7854c67c8bc52a76b5f212a32a4f898c1
Dan Bender <hello@danbender.net>
2016-01-11 06:48:27 +0100

    added LoginViewController in Storyboard, added button to login with Facebook, added LoginViewController and linked the button as IBAction.

---
+++ SwiftTinderClone/LoginViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pressedFBLogin(sender: UIButton) {
+        
+    }
+}
