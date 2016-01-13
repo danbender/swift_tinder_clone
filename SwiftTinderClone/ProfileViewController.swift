d10d6daceed72ddeac6e105a880d82e9d70b5e5b
Dan Bender <hello@danbender.net>
2016-01-13 06:40:03 +0100

    Profile View Controller to storyboard; embedded in NavigationController; linked label and imageview

---
+++ SwiftTinderClone/ProfileViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ }
