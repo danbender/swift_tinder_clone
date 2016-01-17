a9fe444ae26e2b72673048511d6f42c1ff2524a8
Dan Bender <hello@danbender.net>
2016-01-17 14:42:44 +0100

    setup Profile and Cards Nav controller instances.

--- SwiftTinderClone/ViewController.swift
+++ SwiftTinderClone/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class ViewController: UIPageViewController, UIPageViewControllerDataSource {
-
+    
+    let cardsVC: UIViewController! = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("CardsNavController") as! UIViewController
+    let profileVC: UIViewController! = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ProfileNavController") as! UIViewController
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         view.backgroundColor = UIColor.whiteColor()
         dataSource = self
+        self.setViewControllers([cardsVC], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
