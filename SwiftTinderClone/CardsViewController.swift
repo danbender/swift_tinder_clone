85d68031ce13b5e649dac26a5ed737aa2fa1f768
Dan Bender <hello@danbender.net>
2016-01-21 06:22:06 +0100

    when buttons are pressed the frontCard(!) is swiped left (skip) or right (like)

--- SwiftTinderClone/CardsViewController.swift
+++ SwiftTinderClone/CardsViewController.swift
@@ -29,6 +29,10 @@ class CardsViewController: UIViewController, SwipeViewDelegate {
         
         cardStackView.backgroundColor = UIColor.clearColor()
         
+        nahButton.setImage(UIImage(named: "nah-button-pressed"), forState: UIControlState.Highlighted)
+        
+        yeahButton.setImage(UIImage(named: "yeah-button-pressed"), forState: UIControlState.Highlighted)
+        
         fetchUnviewedUsers({
             users in
             self.users = users
@@ -67,9 +71,15 @@ class CardsViewController: UIViewController, SwipeViewDelegate {
     }
     
     @IBAction func nahButtonPressed(sender: UIButton) {
+        if let card = frontCard {
+            card.swipeView.swipe(SwipeView.Direction.Left)
+        }
     }
     
     @IBAction func yeahButtonPressed(sender: UIButton) {
+        if let card = frontCard {
+            card.swipeView.swipe(SwipeView.Direction.Right)
+        }
     }
     
     
