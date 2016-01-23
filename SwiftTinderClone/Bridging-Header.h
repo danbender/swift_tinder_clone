c9cc3f136517f3a03a3329b3f2d1ec6e2a324d6e
Dan Bender <hello@danbender.net>
2016-01-23 20:04:15 +0100

    add ChatViewController as subclass of JSQMessagesViewController (which itself is a subclass of UIViewController); import JSQMessagesViewController via header file

--- SwiftTinderClone/Bridging-Header.h
+++ SwiftTinderClone/Bridging-Header.h
@@ -1,3 +1,4 @@
 #import <Parse/Parse.h>
 #import <ParseFacebookUtils/PFFacebookUtils.h>
 #import "FacebookSDK/FacebookSDK.h"
+#import "JSQMessagesViewController/JSQMessages.h"
---
+++ SwiftTinderClone/ChatViewController.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class ChatViewController: JSQMessagesViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
