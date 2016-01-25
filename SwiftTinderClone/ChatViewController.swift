bf9550728bb71dfd4e83fe3492db7397ac13c74e
Dan Bender <hello@danbender.net>
2016-01-25 06:18:50 +0100

    begin setting up JSQMessenger VC

--- SwiftTinderClone/ChatViewController.swift
+++ SwiftTinderClone/ChatViewController.swift
@@ -1,8 +1,52 @@
 import Foundation
 
 class ChatViewController: JSQMessagesViewController {
+    
+    var messages: [JSQMessage] = []
+    
+    override var senderId: String! {
+        get {
+            return currentUser()!.id
+        }
+        set {
+            super.senderId = newValue
+        }
+    }
+    
+    override var senderDisplayName: String! {
+        get {
+            return currentUser()!.name
+        }
+        
+        set {
+            super.senderDisplayName = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
+//    
+//    func senderDisplayName() -> String! {
+//        return currentUser()!.id
+//    }
+//    
+//    func senderId() -> String! {
+//        return currentUser()!.id
+//    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        var data = self.messages[indexPath.row]
+        return data
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    
+    
+    
 }
