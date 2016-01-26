ce86d16f8469271a61d972385ffc78924255180c
Dan Bender <hello@danbender.net>
2016-01-26 05:56:14 +0100

    add some helper func to Message, a date formatter and a function that helps in saving messages

--- SwiftTinderClone/Message.swift
+++ SwiftTinderClone/Message.swift
@@ -9,3 +9,12 @@ struct Message {
 private let ref = Firebase(url: "https://swifttindercloneios.firebaseio.com/messages")
 private let dateFormat = "yyyyMMddHHmmss"
 
+private func dateFormatter() -> NSDateFormatter {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = dateFormat
+    return dateFormatter
+}
+
+func saveMessage(matchID: String, message: Message) {
+    ref.childByAppendingPath(matchID).updateChildValues([dateFormatter().stringFromDate(message.date) : ["message" : message.message, "sender" : message.senderID]])
+}
